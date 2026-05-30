89106309a1b15897c05bd56fc1e4c9cd12761868
Ratheesh Kumar <ratheeshdevops@gmail.com>
2026-05-30 20:02:12 +0530

    post sudo errors

--- src/main/java/com/example/helloworldrest/HelloWorldController.java
+++ src/main/java/com/example/helloworldrest/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World from Spring Boot.. Hello";
+        return "Hello World from Spring Boot. Hello";
     }
 }
